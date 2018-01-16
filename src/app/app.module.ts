52e91f2fdcc7bdc8df235f40da5e8733c5bffbf3
Andrej-k <andrej.kasic7@gmail.com>
2018-01-16 23:51:00 +0100

    Implement todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
-import { AppComponent } from './app.component';
+// import {AppComponent} from './app.component';
+import {TodosComponent} from './todos/todos.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        TodosComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [TodosComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
